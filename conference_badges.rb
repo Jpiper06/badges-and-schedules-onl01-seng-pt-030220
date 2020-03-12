eb02536ee58e48c143c34d01f0d4d35a93544a9b
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-12 01:29:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,40 @@
-# Write your code here.
+
+ 
+def badge_maker(speaker) 
+    return "Hello, my name is #{speaker}."
+end  
+  
+  def batch_badge_creator(speakers)
+    badge = [0]
+     speaker.each do |speaker|
+      badges << badge_maker (speaker)
+  end 
+    badges
+end
+
+def assign_rooms(speakers)
+  assign = [0]   
+  speakers.each_with_index do |speaker
+speaker, room|
+   assign << "Hello, #{"speaker}! 
+ You'll be assigned to room #{room +1 }!"
+ end
+  assign
+
+end
+
+def printer (speakers)
+  speakers.each_with_index
+  puts
+batch_badge_creatpr(speakers)
+  puts assign_rooms(speaker)
+end 
+      
+
+
+
+
+
+
+    
+  
